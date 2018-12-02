30ec837694797e9ba7c1d9322f4c5c7ab0a9b43e
elpinal <elpinal@users.noreply.github.com>
2018-12-02 11:39:05 +0900

    Add decode_u32

--- src/vm.rs
+++ src/vm.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::fs;
 use std::io;
 use std::io::Read;
+use std::slice;
 
 use failure::Error;
 
@@ -31,6 +32,10 @@ pub enum ExecutionError {
     /// No result in the result register, `R0`.
     #[fail(display = "no result in the result register")]
     NoResult,
+
+    /// 32-bit integers should have 4 bytes, but there are fewer bytes.
+    #[fail(display = "truncated 32-bit integer")]
+    TruncatedU32,
 }
 
 /// Executes a file.
@@ -59,10 +64,10 @@ struct File(HashMap<Reg, u32>);
 impl File {
     /// Executes a sequence of bytes.
     pub fn execute_bytes(&mut self, v: Vec<u8>) -> Result<(), ExecutionError> {
-        let mut iter = v.into_iter();
+        let mut iter = v.iter();
         match iter.next() {
             None => return Err(ExecutionError::MissingVersion),
-            Some(b) => {
+            Some(&b) => {
                 if b != version::BYTE_VERSION {
                     return Err(ExecutionError::VersionMismatch { version: b });
                 }
@@ -70,7 +75,7 @@ impl File {
         }
         match iter.next() {
             None => Err(ExecutionError::UnexpectedEndOfProgram),
-            Some(b) => {
+            Some(&b) => {
                 self.0.insert(Reg(0), b.into());
                 Ok(())
             }
@@ -80,4 +85,14 @@ impl File {
     fn get(&self, r: Reg) -> Option<u32> {
         self.0.get(&r).map(|&x| x)
     }
+}
+
+fn decode_u32(mut iter: slice::Iter<u8>) -> Result<u32, ExecutionError> {
+    let mut u: u32 = 0;
+    for _ in 0..4 {
+        let n: u32 = (*iter.next().ok_or(ExecutionError::TruncatedU32)?).into();
+        u <<= 8;
+        u += n;
+    }
+    Ok(u)
 }
